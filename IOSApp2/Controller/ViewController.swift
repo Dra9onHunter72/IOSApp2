6760ee98642e18fa9d021fc6afd1706642af2ece
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-10-12 13:34:09 -0600

    I put createRandomColor in a new group and file

--- IOSApp2/Controller/ViewController.swift
+++ IOSApp2/Controller/ViewController.swift
@@ -11,26 +11,14 @@ import UIKit
 class ViewController: UIViewController
 {
     
+    private lazy var colorTool : ColorTools = ColorTools()
     @IBOutlet weak var FirstButton: UIButton!
     
     @IBAction func FirstMethod(_ sender: UIButton)
     {
-        FirstButton.backgroundColor = createRandomColor()
-        view.backgroundColor = createRandomColor()
-        FirstButton.setTitleColor(createRandomColor(), for: .normal)
-    }
-
-    
-    
-    private func createRandomColor() -> UIColor
-    {
-        let newColor :UIColor
-        let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
-        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
-        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
-        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
-        
-        return newColor
+        FirstButton.backgroundColor = colorTool.createRandomColor()
+        view.backgroundColor = colorTool.createRandomColor()
+        FirstButton.setTitleColor(colorTool.createRandomColor(), for: .normal)
     }
     
     override func viewDidLoad() {
