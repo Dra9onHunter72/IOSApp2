9d4fdebe86ab51c4aa9f5e576f451d9d89b63d83
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2017-10-18 12:47:40 -0600

    I made it so that you can now view the 3 images I put in

--- IOSApp2/Controller/MediaController.swift
+++ IOSApp2/Controller/MediaController.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class MediaController : UIViewController
+{
+    
+    @IBOutlet weak var ViewImage: UIImageView!
+    @IBOutlet weak var ImageButtonClick: UIButton!
+    @IBOutlet weak var SoundButton: UIButton!
+    @IBOutlet weak var SoundSlider: UISlider!
+    
+    private lazy var color: ColorTools = ColorTools()
+    private var imageCounter = 1
+    
+    
+    @IBAction func imageButtonClick() -> Void
+    {
+        changeImage()
+        view.backgroundColor = color.createRandomColor()
+    }
+    private func changeImage() -> Void
+    {
+        if (imageCounter > 2)
+        {
+            imageCounter = 0
+        }
+        
+        if (imageCounter == 0)
+        {
+            ViewImage.image = UIImage(named: "Leviathan")
+        }
+        else if (imageCounter == 1)
+        {
+            ViewImage.image = UIImage(named: "skull")
+        }
+        else
+        {
+            ViewImage.image = UIImage(named: "lego man")
+        }
+    }
+    
+}
